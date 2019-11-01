0ccd607e37fdb488df6d4c9082e7a5990d613292
Lucas S <lucas.soaresouza@gmail.com>
2019-11-01 10:47:51 -0300

    Adding notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Notification(models.Model):
+    token = models.CharField(max_length=255, default="")
+    title = models.CharField(max_length=100, blank=False)
+    message = models.CharField(max_length=255, blank=False)
+    date = models.DateTimeField(default=timezone.now)
