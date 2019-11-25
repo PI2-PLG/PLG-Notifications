4afc2d5e367efea001e16001c35974acb52a19ac
Lucas S <lucas.soaresouza@gmail.com>
2019-11-25 20:56:11 -0300

    Changing notification model

--- notifications/helper.py
+++ notifications/helper.py
@@ -5,10 +5,33 @@ from exponent_server_sdk import PushResponseError
 from exponent_server_sdk import PushServerError
 from requests.exceptions import ConnectionError
 from requests.exceptions import HTTPError
+from notifications.models import Notification
+
+def create_notification(module):
+    title = ''
+    message = ''
+    if(module["module_status"] == "OFFLINE"):
+        title = f'[{module["module_name"]}] Módulo Offline!'
+        message = f'Não foi possível localizar o {module["module_name"]}!'
+        print(f'[LOG] {module["module_name"]} is OFFLINE')
+    elif(module["module_status"] == "IN_MOTION"):
+        title = f'[{module["module_name"]}] Em Movimento!'
+        message = f'O sistema detectou que {module["module_name"]} aparentmente está em movimento!'
+        print(f'[LOG] {module["module_name"]} is IN_MOTION')
+    elif(module["module_status"] == "FIRERISK"):
+        title = f'[{module["module_name"]}] Risco de Queimada!'
+        message = f'O sistema detectou uma possível queimada! Verifique o {module["module_name"]}.'
+        print(f'[LOG] {module["module_name"]} is in FIRERISK')
+
+    notification = Notification(
+                                title=title,
+                                message=message,
+                                module_name=module_name,
+                                )
+    notification.save()
 
 def send_notification(tokens, message, extra=None):
     for token in tokens:
-        print("AQUUII " + token)
         try:
             response = PushClient().publish(
                 PushMessage(to=token,
